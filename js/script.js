d9c5c026542cb1fc704ebd5073e323ed677098a6
Pranita Ramakrishnan <pranitar@andrew.cmu.edu>
2015-09-14 16:23:28 -0400

    More mockups updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
+// $(function() {
+// 	// $('#choose-existing').click(function() {
+// 	// 	$('#homescreen').css('visibility','hidden');
+// 	// 	$('#dashboard').css('visibility','visible');
+// 	// })
+// })
+
 $(function() {
-	// $('#choose-existing').click(function() {
-	// 	$('#homescreen').css('visibility','hidden');
-	// 	$('#dashboard').css('visibility','visible');
-	// })
-})
+    $( "#slider-vertical" ).slider({
+      orientation: "vertical",
+      range: "min",
+      min: 0,
+      max: 100,
+      value: 60,
+      slide: function( event, ui ) {
+        $( "#amount" ).val( ui.value );
+      }
+    });
+    $( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
+  });
