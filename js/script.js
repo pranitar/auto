ed3f7bcf674c57aff75caadd0a6537cc822127d7
Pranita Ramakrishnan <pranitar@andrew.cmu.edu>
2015-09-15 13:59:32 -0400

    Layout completed but still working on background

--- js/script.js
+++ js/script.js
@@ -5,36 +5,54 @@
 // 	// })
 // })
 
-// $(document).ready(function() {
+$(document).ready(function() {
+    $('#airslider').slider({
+      orientation: "vertical",
+      range: "min",
+      min: 0,
+      max: 100,
+      value: 60,
+      slide: function( event, ui ) {
+        $( "#amount1" ).val( ui.value );
+      }
+    });
+    $( "#amount1" ).val( $( "#airslider" ).slider( "value" ) );
 
-// 	$("#slider-vertical").slider({
-//       orientation: "vertical",
-//       range: "min",
-//       min: 0,
-//       max: 100,
-//       value: 60,
-//       slide: function( event, ui ) {
-//         $( "#amount" ).val( ui.value );
-//       }
-//     });
-//     $( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
+    $('#tempslider').slider({
+      orientation: "vertical",
+      range: "min",
+      min: 0,
+      max: 100,
+      value: 60,
+      slide: function( event, ui ) {
+        $( "#amount2" ).val( ui.value );
+      }
+    });
+    $( "#amount2" ).val( $( "#tempslider" ).slider( "value" ) );
 
-//     $("[name='my-checkbox']").bootstrapSwitch();
+    $('#volumeslider').slider({
+      orientation: "vertical",
+      range: "min",
+      min: 0,
+      max: 100,
+      value: 60,
+      slide: function( event, ui ) {
+        $( "#amount3" ).val( ui.value );
+      }
+    });
+    $( "#amount3" ).val( $( "#volumeslider" ).slider( "value" ) );
 
-//     //Code
-// });
-
-  //   $('#slider-vertical').slider({
-  //     orientation: "vertical",
-  //     range: "min",
-  //     min: 0,
-  //     max: 100,
-  //     value: 60,
-  //     slide: function(event, ui) {
-  //       $('#amount').val(ui.value);
-  //     }
-  //   });
-  //   $('#amount').val($('#slider-vertical').slider("value"));
-  // });
+    $('#orientationslider').slider({
+      orientation: "vertical",
+      range: "min",
+      min: 0,
+      max: 100,
+      value: 60,
+      slide: function( event, ui ) {
+        $( "#amount4" ).val( ui.value );
+      }
+    });
+    $( "#amount4" ).val( $( "#orientationslider" ).slider( "value" ) );
+  });
 
   
